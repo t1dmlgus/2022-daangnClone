4a32b9c9dfb6cc17e971c465efae858eacebf422
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-11 01:00:28 +0900

    feat: 회원가입 컨트롤러 구현

--- back/src/main/java/com/t1dmlgus/daangnClone/user/ui/UserApiController.java
+++ back/src/main/java/com/t1dmlgus/daangnClone/user/ui/UserApiController.java
@@ -1,7 +1,30 @@
 package com.t1dmlgus.daangnClone.user.ui;
 
+import com.t1dmlgus.daangnClone.user.application.UserService;
+import com.t1dmlgus.daangnClone.user.application.UserServiceImpl;
+import com.t1dmlgus.daangnClone.user.ui.dto.JoinRequestDto;
+import com.t1dmlgus.daangnClone.user.ui.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 @RestController
 public class UserApiController {
+
+    private final UserServiceImpl userServiceImpl;
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> join(@RequestBody JoinRequestDto joinRequestDto){
+
+        ResponseDto<?> join = userServiceImpl.join(joinRequestDto);
+
+        return new ResponseEntity<>(join, HttpStatus.OK);
+    }
 }
