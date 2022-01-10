fe0892ab0e02298e89abff3b394154c808a95f41
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-10 22:04:17 +0900

    feat: UserEntity, UserRepository 구현

--- back/src/main/java/com/t1dmlgus/daangnClone/user/domain/User.java
+++ back/src/main/java/com/t1dmlgus/daangnClone/user/domain/User.java
@@ -1,4 +1,38 @@
 package com.t1dmlgus.daangnClone.user.domain;
 
-public class User {
+import com.t1dmlgus.daangnClone.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String phone;
+
+    private String nickName;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+
 }
