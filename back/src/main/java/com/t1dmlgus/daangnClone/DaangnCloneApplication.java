8b71badeb5696cd2ef2b0fb54fa29b52d25150aa
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-10 17:05:04 +0900

    init: 백엔드 개발 환경 구축

---
+++ back/src/main/java/com/t1dmlgus/daangnClone/DaangnCloneApplication.java
@@ -0,0 +1,13 @@
+package com.t1dmlgus.daangnClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaangnCloneApplication.class, args);
+	}
+
+}
