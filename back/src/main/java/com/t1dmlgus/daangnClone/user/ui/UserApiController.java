8ab99c11eb4734e5f8d1f43f755b8be84056d5a2
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-14 11:15:17 +0900

    feat: 회원조회 컨트롤러 구현

--- back/src/main/java/com/t1dmlgus/daangnClone/user/ui/UserApiController.java
+++ back/src/main/java/com/t1dmlgus/daangnClone/user/ui/UserApiController.java
@@ -1,30 +1,34 @@
 package com.t1dmlgus.daangnClone.user.ui;
 
 import com.t1dmlgus.daangnClone.user.application.UserService;
-import com.t1dmlgus.daangnClone.user.application.UserServiceImpl;
 import com.t1dmlgus.daangnClone.user.ui.dto.JoinRequestDto;
 import com.t1dmlgus.daangnClone.user.ui.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 @RestController
 public class UserApiController {
 
-    private final UserServiceImpl userServiceImpl;
+    private final UserService userService;
 
 
     @PostMapping("/signup")
     public ResponseEntity<?> join(@RequestBody JoinRequestDto joinRequestDto){
 
-        ResponseDto<?> join = userServiceImpl.join(joinRequestDto);
+        ResponseDto<?> joinUser = userService.join(joinRequestDto);
 
-        return new ResponseEntity<>(join, HttpStatus.CREATED);
+        return new ResponseEntity<>(joinUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> enquiryUser(@PathVariable Long userId) {
+
+        ResponseDto<?> enquiryUser = userService.enquiry(userId);
+
+        return new ResponseEntity<>(enquiryUser, HttpStatus.OK);
     }
 }
