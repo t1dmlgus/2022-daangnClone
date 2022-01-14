2b35cc81701a8f7fdcf76f16d835aeddc4b7fa48
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-14 11:09:47 +0900

    feat: 글로벌 예외처리 설정

---
+++ back/src/main/java/com/t1dmlgus/daangnClone/handler/exception/CustomApiException.java
@@ -0,0 +1,8 @@
+package com.t1dmlgus.daangnClone.handler.exception;
+
+public class CustomApiException extends RuntimeException{
+
+    public CustomApiException(String message) {
+        super(message);
+    }
+}
