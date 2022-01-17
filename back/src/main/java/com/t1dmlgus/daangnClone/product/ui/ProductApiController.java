5946eccec2daacf9242e2c8de1b5f5c07e7c1477
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-17 10:25:29 +0900

    feat: 상품 컨트롤러 - 상품등록 구현

--- back/src/main/java/com/t1dmlgus/daangnClone/product/ui/ProductApiController.java
+++ back/src/main/java/com/t1dmlgus/daangnClone/product/ui/ProductApiController.java
@@ -1,15 +1,37 @@
 package com.t1dmlgus.daangnClone.product.ui;
 
+import com.t1dmlgus.daangnClone.product.application.ProductService;
+import com.t1dmlgus.daangnClone.user.ui.dto.ResponseDto;
+import com.t1dmlgus.daangnClone.user.ui.dto.product.ProductRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
 @RestController
 public class ProductApiController {
 
-    
+    private final ProductService productService;
+
+    Logger logger = LoggerFactory.getLogger(ProductApiController.class);
+
+    @PostMapping()
+    public ResponseEntity<?> register(ProductRequestDto productRequestDto, @RequestPart(value = "file") MultipartFile file) {   // 유저 추가(세션)
+
+        logger.info("productRequestDto, {}", productRequestDto);
+        logger.info("multipartFile-file, {}", file);
+
+        Long userId = 1L;
+        ResponseDto<?> registerDto = productService.registerProduct(productRequestDto, file);
+
+        return new ResponseEntity<>(registerDto, HttpStatus.CREATED);
+
+    }
 
 
 }
