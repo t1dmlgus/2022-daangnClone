1045f2194a62e52d2ef027868611e841fd0128cf
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-17 21:36:46 +0900

    feat: 컨트롤러 테스트, Spring REST Docs 설정

--- back/src/test/java/com/t1dmlgus/daangnClone/product/ui/ProductApiControllerTest.java
+++ back/src/test/java/com/t1dmlgus/daangnClone/product/ui/ProductApiControllerTest.java
@@ -2,35 +2,34 @@ package com.t1dmlgus.daangnClone.product.ui;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.t1dmlgus.daangnClone.product.application.ProductService;
-import com.t1dmlgus.daangnClone.user.ui.UserApiController;
 import com.t1dmlgus.daangnClone.user.ui.dto.ResponseDto;
-import com.t1dmlgus.daangnClone.user.ui.dto.product.ProductRequestDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.mock.web.MockPart;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.t1dmlgus.daangnClone.common.ApiDocumentUtils.getDocumentRequest;
+import static com.t1dmlgus.daangnClone.common.ApiDocumentUtils.getDocumentResponse;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doReturn;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@ExtendWith(RestDocumentationExtension.class)
 @WebMvcTest(value = ProductApiController.class)
 class ProductApiControllerTest {
 
@@ -43,17 +42,18 @@ class ProductApiControllerTest {
     private ProductService productService;
 
     @BeforeEach
-    void setup(WebApplicationContext webApplicationContext) {
+    void setup(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
                 .build();
-        
+
     }
     
     @DisplayName("컨트롤러 - 상품등록 테스트")
     @Test
     public void registerProductTest() throws Exception{
         //given
-        MockMultipartFile file2 = new MockMultipartFile("file", "파일명.jpeg", "image/jpeg", "파일12".getBytes());
+        MockMultipartFile file2 = new MockMultipartFile("file", "파일명.jpeg", "image/jpeg", "이미지파일".getBytes());
         doReturn(new ResponseDto<>("상품이 등록되었습니다.", 16L))
                 .when(productService).registerProduct(any(), any());
 
@@ -71,7 +71,9 @@ class ProductApiControllerTest {
         //then
         resultActions
                 .andExpect(status().isCreated())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("{class-name}/{method-name}", getDocumentRequest(), getDocumentResponse()
+                ));
     }
 
     
