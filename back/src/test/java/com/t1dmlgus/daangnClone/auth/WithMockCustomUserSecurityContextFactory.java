3529fe551ddc91142b163d7c6de4b19de5ed2fdb
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-18 15:41:00 +0900

    test: 상품도메인 인증에 따른 테스트 리펙토링, WithMockCustomUser 설정

---
+++ back/src/test/java/com/t1dmlgus/daangnClone/auth/WithMockCustomUserSecurityContextFactory.java
@@ -0,0 +1,29 @@
+package com.t1dmlgus.daangnClone.auth;
+
+import com.t1dmlgus.daangnClone.auth.domain.PrincipalDetails;
+import com.t1dmlgus.daangnClone.user.domain.Role;
+import com.t1dmlgus.daangnClone.user.domain.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+
+public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithMockCustomUser> {
+
+    @Override
+    public SecurityContext createSecurityContext(WithMockCustomUser customUser) {
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+        User testUser = new User(1L,"test@gmail.com","1234","이의현","1234-1234","t1dmlgus", Role.ROLE_USER);
+
+        PrincipalDetails principal = new PrincipalDetails(testUser);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(principal, principal.getPassword(), principal.getAuthorities());
+        context.setAuthentication(authentication);
+
+
+        return context;
+    }
+}
--- back/src/test/java/com/t1dmlgus/daangnClone/product/ui/ProductApiControllerTest.java
+++ back/src/test/java/com/t1dmlgus/daangnClone/product/ui/ProductApiControllerTest.java
@@ -1,6 +1,8 @@
 package com.t1dmlgus.daangnClone.product.ui;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.t1dmlgus.daangnClone.auth.WithMockCustomUser;
+import com.t1dmlgus.daangnClone.auth.domain.PrincipalDetails;
 import com.t1dmlgus.daangnClone.product.application.ProductService;
 import com.t1dmlgus.daangnClone.user.ui.dto.ResponseDto;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +16,9 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -47,19 +52,23 @@ class ProductApiControllerTest {
                 .apply(documentationConfiguration(restDocumentation))
                 .build();
 
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
     }
+
     
     @DisplayName("컨트롤러 - 상품등록 테스트")
     @Test
+    @WithMockCustomUser
     public void registerProductTest() throws Exception{
         //given
         MockMultipartFile file2 = new MockMultipartFile("file", "파일명.jpeg", "image/jpeg", "이미지파일".getBytes());
         doReturn(new ResponseDto<>("상품이 등록되었습니다.", 16L))
-                .when(productService).registerProduct(any(), any());
+                .when(productService).registerProduct(any(), any(), any());
 
         //when
         ResultActions resultActions = mockMvc.perform(
-                multipart("/api/product")
+                multipart("/api/product/register")
                         .file(file2)
                         .param("title", "상품명")
                         .param("price", "100")
