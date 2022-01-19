9180b1802e94822b3618f396842f73c0dace6505
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-19 18:21:15 +0900

    feat: 인증 정보 및 인덱스 페이지 컨트롤러 구현

--- back/src/main/java/com/t1dmlgus/daangnClone/auth/ui/AuthController.java
+++ back/src/main/java/com/t1dmlgus/daangnClone/auth/ui/AuthController.java
@@ -4,16 +4,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AuthController {
 
+    // 인증 정보
+    @ResponseBody
     @GetMapping("/auth")
     public Authentication auth(){
 
         return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    // 인덱스 페이지
+    @GetMapping("/")
+    public String index(){
+        return "index.html";
     }
 
 }
