dbac407c6deb0aaaf1efebc97d87b14a62028713
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-20 20:15:27 +0900

    feat: 상품 조회 기능 인터페이스 생성

--- back/src/main/java/com/t1dmlgus/daangnClone/product/application/ProductService.java
+++ back/src/main/java/com/t1dmlgus/daangnClone/product/application/ProductService.java
@@ -1,13 +1,25 @@
 package com.t1dmlgus.daangnClone.product.application;
 
+import com.t1dmlgus.daangnClone.product.ui.dto.InquiryProductTopFourResponseDto;
 import com.t1dmlgus.daangnClone.user.domain.User;
 import com.t1dmlgus.daangnClone.user.ui.dto.ResponseDto;
-import com.t1dmlgus.daangnClone.user.ui.dto.product.ProductRequestDto;
+import com.t1dmlgus.daangnClone.product.ui.dto.ProductRequestDto;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 
 public interface ProductService {
 
     // 상품 등록
     ResponseDto<?> registerProduct(ProductRequestDto productRequestDto, MultipartFile multipartFile, User user);
+
+    // 상품 조회
+    ResponseDto<?> inquiryProduct(Long productId);
+
+    // 상품 수정
+
+    // 상품 삭제
+
+
 }
