b27fa241bf98af6a9282e1a60d08c7c8b7ad67e5
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-23 21:29:38 +0900

    feat: 좋아요 아이콘 클릭 시, 좋아요 기능 연동

---
+++ back/src/main/resources/static/js/product.js
@@ -0,0 +1,75 @@
+var like ={
+    init: function(){
+        $('#likebtn').click(function(){           // 좋아요 클릭
+
+                console.log($('#likebtn').children().attr('val'));
+
+                var data = $('#likebtn').attr('val');
+                if($('#likebtn').children().attr('val')=="true"){
+
+                    like.like(data);
+                }else{
+                    like.unlike(data);
+                }
+
+
+                console.log($('#likebtn').children().attr('val')=="true");
+                if($('#likebtn').children().attr('val')=="true"){
+                    $('#likebtn').children().children('img').attr("src","/img/likes.png");
+                }else{
+                    $('#likebtn').children().children('img').attr("src","/img/heart.png");
+                }
+
+            });
+
+    },
+
+        like: function(data){
+             console.log(data);
+
+            $.ajax({
+
+                type: "DELETE",
+                url: "/api/likes/"+data,
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json'
+
+            }).done(function(aa){
+
+                alert("좋아요 취소💔");
+                $('#likebtn').children('div').attr('val','false');
+
+            }).fail(function(error){
+                alert('좋아요 등록 에러');
+            })
+
+
+        },
+        unlike: function(data){
+
+            console.log(data);
+
+            $.ajax({
+
+                type: "POST",
+                url: "/api/likes/"+data,
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8'
+
+            }).done(function(aa){
+                alert("좋아요❤");
+                $('#likebtn').children('div').attr('val','true');
+
+            }).fail(function(error){
+                alert('좋아요 취소 에러');
+
+            })
+
+
+
+        }
+
+
+}
+
+like.init();
