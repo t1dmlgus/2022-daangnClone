e7a6b6ab754ae47568bad8ee77498c01f6fa5885
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-25 16:07:21 +0900

    refactor: 전체 상품 조회 리펙터링
    - 몇 분전 기능 추가
    - 상품 좋아요 정보(상태, 카운트) 추가
    - 커버 이미지 조회 메소드화

--- back/src/main/java/com/t1dmlgus/daangnClone/product/application/ProductServiceImpl.java
+++ back/src/main/java/com/t1dmlgus/daangnClone/product/application/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package com.t1dmlgus.daangnClone.product.application;
 
 import com.t1dmlgus.daangnClone.handler.exception.CustomApiException;
 import com.t1dmlgus.daangnClone.likes.application.LikesService;
+import com.t1dmlgus.daangnClone.likes.ui.dto.ProductLikesStatus;
 import com.t1dmlgus.daangnClone.product.domain.Product;
 import com.t1dmlgus.daangnClone.product.domain.ProductRepository;
 import com.t1dmlgus.daangnClone.product.ui.ProductApiController;
@@ -11,6 +12,7 @@ import com.t1dmlgus.daangnClone.product.ui.dto.InquiryProductTopFourResponseDto;
 import com.t1dmlgus.daangnClone.product.ui.dto.ProductRequestDto;
 import com.t1dmlgus.daangnClone.user.domain.User;
 import com.t1dmlgus.daangnClone.user.ui.dto.ResponseDto;
+import com.t1dmlgus.daangnClone.util.BeforeTime;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +21,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,52 +55,77 @@ public class ProductServiceImpl implements ProductService{
     @Transactional
     @Override
     public ResponseDto<?> inquiryProduct(Long productId, Long userId) {
-        // 1. 상품 get
+
+        // 1. 상품
         Product product = productRepository.findById(productId).orElseThrow(
                 () -> { return new CustomApiException("해당 상품은 없습니다.");});
 
-        // 2. 상품 id -> 상품 이미지리스트 get
-        List<String> productImages = s3Service.inquiryProductImage(productId);
-        // 3. 해당 유저에 관한 상품(top4) get
-        List<InquiryProductTopFourResponseDto> t4Prod = inquiryTopFourProduct(product.getUser().getId());
-        // 4. 상품에 대한 세션유저의 좋아요 상태 확인
-        boolean likesStatus = likesService.checkLikesStatus(productId, userId);
+        // 2. 상품 이미지 리스트
+        List<String> productImages = getProductImages(productId);
 
+        // 3. 해당 유저에 관한 상품 top4
+        List<InquiryProductTopFourResponseDto> topFourProducts = inquiryTopFourProduct(product.getUser().getId());
 
-        return new ResponseDto<>("조회한 상품입니다.", new InquiryProductResponseDto(product, productImages,likesStatus, t4Prod));
+        // 4. 상품 좋아요 정보(상태, 카운트)
+        ProductLikesStatus productLikesStatus = getProductLikesStatus(userId, productId);
+        return new ResponseDto<>("조회한 상품입니다.", new InquiryProductResponseDto(product, getProductImages(productId), productLikesStatus, topFourProducts));
     }
 
 
     @Transactional
     @Override
-    public ResponseDto<?> allProduct() {
+    public ResponseDto<?> allProduct(Long userId) {
 
+        List<AllProductResponseDto> allProductDtos = new ArrayList<>();
 
-        List<AllProductResponseDto> allProductDto = new ArrayList<>();
-        List<Product> allProduct = productRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+        for (Product product : productRepository.findAll(Sort.by(Sort.Direction.DESC, "id"))) {
 
-        for (Product product : allProduct) {
-            String coverImage = s3Service.inquiryProductImage(product.getId()).get(0);
-            allProductDto.add(new AllProductResponseDto(product, coverImage));
+            // 1. 몇분 전
+            String beforeTime = getRegisterProduct(product);
+
+            // 2. 상품 좋아요 정보(상태, 카운트) 조회
+            ProductLikesStatus productLikesStatus = getProductLikesStatus(userId, product.getId());
+
+            // 3. 커버 이미지
+            String coverImage = getCoverImage(product.getId());
+
+            // 4. 랜딩 페이지 List<Dto>
+            allProductDtos.add(new AllProductResponseDto(product, beforeTime, productLikesStatus, coverImage));
         }
-        return new ResponseDto<>("전체 상품 조회힙니다.", allProductDto);
+        return new ResponseDto<>("상품을 전체 조회합니다.", allProductDtos);
+    }
+
+
+    // 몇분 전 기능
+    private String getRegisterProduct(Product product) {
+        return BeforeTime.calculateTime(product.getCreatedDate());
+    }
+
+    // 상품 좋아요 정보(상태, 카운트) 조회
+    protected ProductLikesStatus getProductLikesStatus(Long userId, Long productId) {
+        return likesService.productLikesStatus(productId, userId);
+    }
+
+    // 상품 이미지 리스트 조회
+    protected List<String> getProductImages(Long productId) {
+        return s3Service.inquiryProductImage(productId);
+    }
+
+    // 상품 커버 이미지 조회
+    protected String getCoverImage(Long productId) {
+        return getProductImages(productId).get(0);
     }
 
     // 유저 판매 상폼 조회(top4)
     protected List<InquiryProductTopFourResponseDto> inquiryTopFourProduct(Long userId) {
 
         List<InquiryProductTopFourResponseDto> t4Product = new ArrayList<>();
-        List<Product> products = productRepository.inquiryProductTopFourByUser(userId);
-        for (Product product : products) {
-
-            // 상품 첫번째 이미지
-            String coverImage = s3Service.inquiryProductImage(product.getId()).get(0);
-            t4Product.add(new InquiryProductTopFourResponseDto(product, coverImage));
+        for (Product product : productRepository.inquiryProductTopFourByUser(userId)) {
+            t4Product.add(new InquiryProductTopFourResponseDto(product, getCoverImage(product.getId())));
         }
 
         return t4Product;
     }
-
 
 
 }
