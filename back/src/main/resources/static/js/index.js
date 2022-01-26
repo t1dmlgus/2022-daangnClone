dd697fc9a1e8336b3dec9a750b32b9c8c26d6652
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-26 23:44:16 +0900

    style: 인덱스 페이지 소개글 작성

---
+++ back/src/main/resources/static/js/index.js
@@ -0,0 +1,8 @@
+
+window.addEventListener('load', () => {
+  alert("이 서비스는 당근마켓을 클론한 웹서비스입니다.\nGalaxy S9+(320px, 658px) 기준으로 최적화된 서비스를 사용할 수 있습니다.\n\n서비스 이용 시, 궁금한 사항이나 자세한 사항은 \nhttps://github.com/t1dmlgus/2022-daangnClone를 참고 하시거나\ndmlgusgngl@gmail.com로 문의 주시길 바랍니다.\n감사합니다.");
+}, { once : true});
+//function index() {
+//
+//}
+
