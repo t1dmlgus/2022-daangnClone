8cd75e89f6341f3839c0c82033a973ce4c86f3c9
t1dmlgus <dmlgusgngl@gmail.com>
2022-01-26 19:29:00 +0900

    feat: 카테고리 한글화(enum 타입 getter)

--- back/src/main/java/com/t1dmlgus/daangnClone/product/domain/Category.java
+++ back/src/main/java/com/t1dmlgus/daangnClone/product/domain/Category.java
@@ -1,21 +1,29 @@
 package com.t1dmlgus.daangnClone.product.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum Category {
 
-    DIGITAL_DEVICE,             // 디지털 기기
-    HOME_APPLIANCES,            // 생활가전
-    FURNITURE_INTERIOR,         // 가구/인테리어
-    CHILD,                      // 유아
-    LIFE_PROCESSED_FOOD,        // 생활/가공식품
-    CHILDREN_BOOKS,             // 유아도서
-    WOMEN_CLOTHING,             // 여성의류
-    MEN_CLOTHING,               // 남성의류
-    GAME_HOBBY,                 // 게임/취미
-    BEAUTY,                     // 뷰티
-    PET_SUPPLIES,               // 반려동물용품
-    BOOK_TICKET_ALBUM,          // 도서/티켓/음반
-    PLANT,                      // 식물
-    OTHER_USED_GOODS,           // 기타 중고물품
-    USED_CAR                    // 중고차
+    DIGITAL_DEVICE("디지털기기"),
+    HOME_APPLIANCES("생활가전"),
+    FURNITURE_INTERIOR("가구/인테리어"),
+    CHILD("유아"),
+    LIFE_PROCESSED_FOOD("생활/가공식품"),
+    CHILDREN_BOOKS("유아도서"),
+    WOMEN_CLOTHING("여성의류"),
+    MEN_CLOTHING("남성의류"),
+    GAME_HOBBY("게임/취미"),
+    BEAUTY("뷰티"),
+    PET_SUPPLIES("반려동물용품"),
+    BOOK_TICKET_ALBUM("도서/티켓/음반"),
+    PLANT("식물"),
+    OTHER_USED_GOODS("기타 중고물품"),
+    USED_CAR("중고차");
+
+    private final String krName;
 
 }
